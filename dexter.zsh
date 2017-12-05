c61ea6e7c063d68ad939f06799cb1ab2b5f174e1
Shiv <shvenkat@users.noreply.github.com>
2017-12-05 13:04:16 -0800

    Add exit status indicator, remove horizontal rule, change left prompt.

--- dexter.zsh
+++ dexter.zsh
@@ -2,70 +2,65 @@
 #
 # The prompt looks as follows, where _ is the cursor:
 #
-# ────────────────────────────────────────────────────────────────────────────
-# %% _                                    [env] (master *%) ~/f/b/quux megatr…
+# ▶▶ false                                [env] (master *%) ~/f/b/quux megatr…
+# exit 1
 #
+# ▶▶ true                                 [env] (master *%) ~/f/b/quux megatr…
+#
+# ▶▶ _                                    [env] (master *%) ~/f/b/quux megatr…
 #
 # Features:
 #
-#   * A horizontal line before each prompt.
+#   * A blank line before each prompt. If the previous command exited with a
+#     non-zero status, the blank line is preceeded by a line indicating the exit
+#     status.
 #
-#   * The left prompt indicates:
-#     + exit status of the previous command (green for 0, red otherwise), and
-#     + privilege of the shell (## if privileged, %% otherwise).
+#   * The left prompt indicates the privilege of the shell (## if privileged, ▶▶
+#     otherwise).
 #
 #   * The right prompt shows the following information from left to right, if
 #     the corresponding plugins are available:
-#     + python virtualenv, using the virtualenv plugin (robbyrussell/oh-my-zsh),
-#     + git status, using the gitfast plugin (robbyrussell/oh-my-zsh),
-#     + fish-style abbreviated working directory, using the shrink-path plugin
-#       (shvenkat/oh-my-zsh), and
-#     + abbreviated hostname.
+#       + python virtualenv, using the oh-my-zsh virtualenv plugin,
+#       + git status, using the oh-my-zsh gitfast plugin,
+#       + fish-style abbreviated working directory, using the oh-my-zsh
+#         shrink-path plugin, and
+#       + abbreviated hostname.
 #
 # To facilitate integration with terminal color schemes, the color of certain
 # elements can be customized using the following variables. The default colors
 # shown below are intended for use with the solarized dark palette. To override
 # these, specify any valid terminal/ANSI color escape sequences.
 #
-#   DEXTER_SEPARATOR_COLOR       black        $fg[black]
-#   DEXTER_EXIT_SUCCESS_COLOR    green        $fg[green]
 #   DEXTER_EXIT_FAILURE_COLOR    red          $fg[red]
 #   DEXTER_VENV_COLOR            yellow       $fg[yellow]
 #   DEXTER_GIT_COLOR             violet       $fg_bold[magenta]
 #   DEXTER_WORKDIR_COLOR         normal       $fg[default]
 #   DEXTER_HOSTNAME_COLOR        dark gray    $fg_bold[green]
 #
-# Finally, the line drawing character(s) can be changed using the variable
-# DEXTER_SEPARATOR_CHARS. The default value is the Unicode character 'BOX
-# DRAWINGS HEAVY HORIZONTAL' (U+2501).
-#
-#
 # Copyright (c) 2016 by Shiv Venkatasubrahmanyam <shiv@alum.mit.edu>.
 # License: MIT.
 
 setopt prompt_subst
 
-DEXTER_SEPARATOR_COLOR=${DEXTER_SEPARATOR_COLOR=$fg[black]}
-DEXTER_EXIT_SUCCESS_COLOR=${DEXTER_EXIT_SUCCESS_COLOR=$fg[green]}
 DEXTER_EXIT_FAILURE_COLOR=${DEXTER_EXIT_FAILURE_COLOR=$fg[red]}
 DEXTER_VENV_COLOR=${DEXTER_VENV_COLOR=$fg[yellow]}
 DEXTER_GIT_COLOR=${DEXTER_GIT_COLOR=$fg_bold[magenta]}
 DEXTER_WORKDIR_COLOR=${DEXTER_WORKDIR_COLOR=$fg[default]}
 DEXTER_HOSTNAME_COLOR=${DEXTER_HOSTNAME_COLOR=$fg_bold[green]}
-DEXTER_SEPARATOR_CHARS=${DEXTER_SEPARATOR_CHARS='━'}
 
 
 # LEFT PROMPT
 
 # Use a horizontal line as a separator before each prompt.
-__terminator=
-__separator='${DEXTER_SEPARATOR_COLOR}${(l:$COLUMNS::'$DEXTER_SEPARATOR_CHARS':)__terminator}${reset_color}'
 precmd() {
-    eval "echo $__separator"
+    eval "\
+        if [[ $? -gt 0 ]] && [[ $? -lt 128 ]] || [[ $? -gt 159 ]]; then \
+            echo -e \"${DEXTER_EXIT_FAILURE_COLOR}exit $?${reset_color}\"; \
+        fi; \
+        echo"
 }
-# Use '%% ' if the shell is running without privileges, '## ' otherwise.
-# Use green text if the last command exited successfully, red otherwise.
-PS1='%(?,%{${DEXTER_EXIT_SUCCESS_COLOR}%},%{${DEXTER_EXIT_FAILURE_COLOR}%})%#%# %{$reset_color%}'
+# Use '▶▶ ' if the shell is running without privileges, '## ' otherwise.
+PS1='%(!.#.▶)%(!.#.▶) '
 
 
 # RIGHT PROMPT
