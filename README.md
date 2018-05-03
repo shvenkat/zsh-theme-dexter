0a9906c749130032a726b735d8d05ce3b89386a4
Shiv <shvenkat@users.noreply.github.com>
2018-05-03 14:20:14 -0700

    Version 2: Show exit status and time elapsed; configurable right prompt.

--- README.md
+++ README.md
@@ -1,58 +1,131 @@
 # Dexter, a zsh theme.
 
-A theme with an emphasis on the right side (hence the name) of the terminal.
-
-
-## Examples
-
-Here's how this theme looks with the [solarized]
-(http://ethanschoonover.com/solarized) color palette, in dark and
-light modes.
-
-![Dexter on Solarized Dark](demo_solarized_dark.png)
-
-![Dexter on Solarized Light](demo_solarized_light.png)
-
-
-## Features
-
-* A horizontal line before each prompt.
-
-* The left prompt indicates:
-  + exit status of the previous command (green for 0, red otherwise), and
-  + privilege of the shell (## if privileged, %% otherwise).
-
-* The right prompt shows the following information from left to right, if
-  the corresponding plugins are available:
-  + python virtualenv, using the virtualenv plugin (robbyrussell/oh-my-zsh),
-  + git status, using the gitfast plugin (robbyrussell/oh-my-zsh),
-  + fish-style abbreviated working directory, using the shrink-path plugin
-    (robbyrussell/oh-my-zsh), and
-  + abbreviated hostname.
-
-
-## Customization
-
-The color of certain elements can be customized using the following
-variables. The default colors shown below are intended for use with
-the solarized dark palette. To override these, specify any valid
-terminal/ANSI color escape sequences.
-
-    DEXTER_SEPARATOR_COLOR       black        $fg[black]
-    DEXTER_EXIT_SUCCESS_COLOR    green        $fg[green]
-    DEXTER_EXIT_FAILURE_COLOR    red          $fg[red]
-    DEXTER_VENV_COLOR            yellow       $fg[yellow]
-    DEXTER_GIT_COLOR             violet       $fg_bold[magenta]
-    DEXTER_WORKDIR_COLOR         normal       $fg[default]
-    DEXTER_HOSTNAME_COLOR        dark gray    $fg_bold[green]
-
-The line drawing character(s) can be changed using the variable
-DEXTER_SEPARATOR_CHARS. The default value is the Unicode character
-'BOX DRAWINGS HEAVY HORIZONTAL' (U+2501).
+A theme with an emphasis on the right side of the terminal. It works best with
+terminals wider than the standard 80 columns.
 
 
 ## License
 
 Copyright (c) 2016 by Shiv Venkatasubrahmanyam <shiv@alum.mit.edu>.
+License: Apache License 2.0
 
-License: MIT.
+
+## Examples
+
+Here's how this theme looks with the
+[solarized](http://ethanschoonover.com/solarized) light color palette.
+
+With default settings:
+![Dexter with default settings](demo_default_solarized_light.png)
+
+With some custom settings:
+![Dexter with some custom settings](demo_custom_solarized_light.png)
+
+With simple settings:
+![Dexter with simple settings](demo_simple_solarized_light.png)
+
+
+## Features
+
+  * A blank line is printed before each prompt to visually separate successive
+    commands.
+
+  * The left prompt is simple and of fixed length, aligning and visually marking
+    the commands, making them easy to distinguish from command output on the
+    screen. The default is a diamond `◀▶`.
+
+  * The right prompt can show the following information, if configured and the
+    corresponding plugins are available:
+      + python virtualenv, using the oh-my-zsh `virtualenv` plugin;
+      + git status, using the oh-my-zsh `gitfast` plugin;
+      + abbreviated working directory, using the oh-my-zsh `shrink-path` plugin;
+      + abbreviated hostname; and
+      + time of day in 24-hour format (hh:mm).
+    The selection and color of elements in the right prompt can be customized.
+
+  * Information about the previous command, if relevant, is shown within
+    parentheses after the last line of command output.
+      + If the previous command exited with a non-zero status or exited due to a
+        signal, the exit status or signal is shown in red.
+      + If the previous command took longer than a configurable time limit to
+        run, the wall clock time elapsed in shown in suitable units.
+
+    Alternatively, the color of the left prompt can indicate non-zero exit.
+
+
+## Installation and Use
+
+To use this plugin, in your `~/.zshrc`:
+
+ 1. Source the plugins you need for the right prompt,
+ 2. Set the `DEXTER_*` configuration variables described below, and
+ 3. Source this file.
+
+A plugin manager is highly recommended to simplify installation and loading.
+For instance, using [zplug](https://github.com/zplug/zplug) and the following,
+you could customize your shell to look like the "custom" example above.
+
+    ...  # Load zplug.
+    zplug "plugins/shrink-path", from:oh-my-zsh
+    zplug "plugins/gitfast", from:oh-my-zsh, defer:1
+    zplug "plugins/virtualenv", from:oh-my-zsh
+    zplug "shvenkat/zsh-theme-dexter", use:"*.zsh", defer:3
+    DEXTER_RIGHT_PROMPT_ELEMS=(time workdir git venv)
+    DEXTER_WORKDIR_COLOR=$fg_bold[green]
+    DEXTER_GIT_COLOR=$fg_bold[magenta]
+    DEXTER_VENV_COLOR=$fg[yellow]
+    DEXTER_SHOW_EXIT_MESSAGE=0
+    DEXTER_SHOW_ELAPSED_TIME=0
+    ...
+    zplug load
+
+
+## Customization
+
+To customize this theme, set the following variables before sourcing this
+file. Default values are shown below.
+
+    DEXTER_LEFT_PROMPT_STRING=◀▶           # No trailing space.
+
+The prompt string. __A space will be printed to the right of this string__.
+
+    DEXTER_RIGHT_PROMPT_ELEMS=(venv git workdir host time)
+
+The sequence of elements in the right prompt. Use `()` to disable the right
+prompt.
+
+    DEXTER_VENV_COLOR=$fg_bold[green]      # Darker than foreground.
+    DEXTER_GIT_COLOR=$fg[default]`         # Foreground text color.
+    DEXTER_WORKDIR_COLOR=$fg_bold[cyan]`   # Lighter than foreground.
+    DEXTER_HOST_COLOR=$fg_bold[cyan]`      # Lighter than foreground.
+    DEXTER_TIME_COLOR=$fg_bold[cyan]`      # Lighter than foreground.
+
+The color of right prompt elements. The default colors are intended for use with
+the solarized light palette. Any terminal/ANSI color specification supported by
+zsh or your terminal can be used.
+
+    DEXTER_SHOW_EXIT_MESSAGE=1
+
+If set to `1`, the non-zero exit status of the last command is shown within
+parentheses below command output. If zero, non-zero exit is indicated by the
+color of the left prompt.
+
+    DEXTER_EXIT_STATUS_COLOR=$fg[red]
+
+The color used to indicate non-zero exit of the previous command. It is used
+either in the exit status message if `DEXTER_SHOW_EXIT_MESSAGE` is `1`, or in
+the left prompt.
+
+    DEXTER_SHOW_ELAPSED_TIME=1
+    DEXTER_TIME_LIMIT_SECS=5
+
+If set to `1`, the wall-clock time consumed by the last command, if longer than
+`DEXTER_TIME_LIMIT_SECS`, is shown within parentheses below command output.
+
+    DEXTER_UNTIMED_COMMANDS=("more" "less" "emacs" "emacsclient" "nvim" "vim"
+                             "man" "git" "time")
+
+The commands for which elapsed time is not shown. These are typically
+interactive programs. Elements of this array are strings -- possibly with more
+than one word -- that match the beginning of excluded commands. Matching is
+performed _after alias expansion_.
--- dexter.zsh
+++ dexter.zsh
@@ -1,98 +1,216 @@
-# A theme with an emphasis on the right side (hence the name) of the terminal.
+# A theme with an emphasis on the right side of the terminal, hence the name.
+#
+# Copyright (c) 2016 by Shiv Venkatasubrahmanyam <shiv@alum.mit.edu>.
+# License: Apache License 2.0
+#
+# EXAMPLE
 #
 # The prompt looks as follows, where _ is the cursor:
 #
-# ▶▶ false                                [env] (master *%) ~/f/b/quux megatr…
-# exit 1
+# ◀▶ sleep 5                         [env] (master *%) ~/f/b/quux megatr… 18:17
+# (5s)
 #
-# ▶▶ true                                 [env] (master *%) ~/f/b/quux megatr…
+# ◀▶ false                           [env] (master *%) ~/f/b/quux megatr… 18:18
+# (exit 1)
 #
-# ▶▶ _                                    [env] (master *%) ~/f/b/quux megatr…
+# ◀▶ _                               [env] (master *%) ~/f/b/quux megatr… 18:18
 #
-# Features:
-#
-#   * A blank line before each prompt. If the previous command exited with a
-#     non-zero status, the blank line is preceeded by a line indicating the exit
-#     status.
-#
-#   * The left prompt indicates the privilege of the shell (## if privileged, ▶▶
-#     otherwise).
-#
-#   * The right prompt shows the following information from left to right, if
-#     the corresponding plugins are available:
-#       + python virtualenv, using the oh-my-zsh virtualenv plugin,
-#       + git status, using the oh-my-zsh gitfast plugin,
-#       + fish-style abbreviated working directory, using the oh-my-zsh
-#         shrink-path plugin, and
-#       + abbreviated hostname.
-#
-# To facilitate integration with terminal color schemes, the color of certain
-# elements can be customized using the following variables. The default colors
-# shown below are intended for use with the solarized dark palette. To override
-# these, specify any valid terminal/ANSI color escape sequences.
-#
-#   DEXTER_EXIT_FAILURE_COLOR    red          $fg[red]
-#   DEXTER_VENV_COLOR            yellow       $fg[yellow]
-#   DEXTER_GIT_COLOR             violet       $fg_bold[magenta]
-#   DEXTER_WORKDIR_COLOR         normal       $fg[default]
-#   DEXTER_HOSTNAME_COLOR        dark gray    $fg_bold[green]
-#
-# Copyright (c) 2016 by Shiv Venkatasubrahmanyam <shiv@alum.mit.edu>.
-# License: MIT.
+# See README.md for features, installation, use and customization.
+
+
+# ----------  DEFAULTS  ----------
+
+DEXTER_LEFT_PROMPT_STRING="${DEXTER_LEFT_PROMPT_STRING:-◀▶}"
+if ! [[ -v DEXTER_RIGHT_PROMPT_ELEMS ]]; then
+    DEXTER_RIGHT_PROMPT_ELEMS=(venv git workdir host time)
+fi
+DEXTER_VENV_COLOR="${DEXTER_VENV_COLOR:-$fg_bold[green]}"
+DEXTER_GIT_COLOR="${DEXTER_GIT_COLOR:-$fg[default]}"
+DEXTER_WORKDIR_COLOR="${DEXTER_WORKDIR_COLOR:-$fg_bold[cyan]}"
+DEXTER_HOST_COLOR="${DEXTER_HOST_COLOR:-$fg_bold[cyan]}"
+DEXTER_TIME_COLOR="${DEXTER_TIME_COLOR:-$fg_bold[cyan]}"
+DEXTER_SHOW_EXIT_MESSAGE="${DEXTER_SHOW_EXIT_MESSAGE:-1}"
+DEXTER_EXIT_STATUS_COLOR="${DEXTER_EXIT_STATUS_COLOR:-$fg[red]}"
+DEXTER_SHOW_ELAPSED_TIME="${DEXTER_SHOW_ELAPSED_TIME:-1}"
+DEXTER_TIME_LIMIT_SECS="${DEXTER_TIME_LIMIT_SECS:-5}"
+if ! [[ -v DEXTER_UNTIMED_COMMANDS ]]; then
+    DEXTER_UNTIMED_COMMANDS=("more" "less" "emacs" "emacsclient" "nvim" "vim"
+                             "man" "git" "time")
+fi
+
+
+# ----------  LEFT PROMPT  ----------
 
 setopt prompt_subst
-
-DEXTER_EXIT_FAILURE_COLOR=${DEXTER_EXIT_FAILURE_COLOR=$fg[red]}
-DEXTER_VENV_COLOR=${DEXTER_VENV_COLOR=$fg[yellow]}
-DEXTER_GIT_COLOR=${DEXTER_GIT_COLOR=$fg_bold[magenta]}
-DEXTER_WORKDIR_COLOR=${DEXTER_WORKDIR_COLOR=$fg[default]}
-DEXTER_HOSTNAME_COLOR=${DEXTER_HOSTNAME_COLOR=$fg_bold[green]}
-
-
-# LEFT PROMPT
-
-# Use a horizontal line as a separator before each prompt.
-precmd() {
-    eval "\
-        if [[ $? -gt 0 ]] && [[ $? -lt 128 ]] || [[ $? -gt 159 ]]; then \
-            echo -e \"${DEXTER_EXIT_FAILURE_COLOR}exit $?${reset_color}\"; \
-        fi; \
-        echo"
-}
-# Use '▶▶ ' if the shell is running without privileges, '## ' otherwise.
-PS1='%(!.#.◀)%(!.#.▶) '
+if [[ "$DEXTER_SHOW_EXIT_MESSAGE" == 1 ]]; then
+    PS1='${DEXTER_LEFT_PROMPT_STRING} '
+else
+    PS1='%(?,%{${reset_color}%},%{${DEXTER_EXIT_STATUS_COLOR}%})${DEXTER_LEFT_PROMPT_STRING}%{$reset_color%} '
+fi
 
 
-# RIGHT PROMPT
+# ----------  RIGHT PROMPT  ----------
 
 RPS1=''
-
-# Python virtualenv.
-if type virtualenv_prompt_info >/dev/null 2>&1; then
-    ZSH_THEME_VIRTUALENV_PREFIX="["
-    ZSH_THEME_VIRTUALENV_SUFFIX="]"
-    RPS1+='%{${DEXTER_VENV_COLOR}%}$(virtualenv_prompt_info)%{$reset_color%} '
+for _elem in $DEXTER_RIGHT_PROMPT_ELEMS; do
+    case "$_elem" in
+        venv)
+            # Python virtualenv.
+            if type virtualenv_prompt_info >/dev/null 2>&1; then
+                ZSH_THEME_VIRTUALENV_PREFIX="["
+                ZSH_THEME_VIRTUALENV_SUFFIX="]"
+                RPS1+=' %{${DEXTER_VENV_COLOR}%}$(virtualenv_prompt_info)%{${reset_color}%}'
+            fi
+            ;;
+        git)
+            # Git status
+            if type __git_ps1 >/dev/null 2>&1; then
+                GIT_PS1_SHOWDIRTYSTATE=yes
+                GIT_PS1_SHOWSTASHSTATE=yes
+                GIT_PS1_SHOWUNTRACKEDFILES=yes
+                GIT_PS1_SHOWUPSTREAM="verbose"
+                GIT_PS1_DESCRIBE_STYLE="branch"
+                RPS1+=' %{${DEXTER_GIT_COLOR}%}$(__git_ps1 "(%s)")%{${reset_color}%}'
+            fi
+            ;;
+        workdir)
+            # Working directory.
+            if type shrink_path >/dev/null 2>&1; then
+                # Use a fish-style abbreviation.
+                RPS1+=' %{${DEXTER_WORKDIR_COLOR}%}$(shrink_path -l -t -T)%{${reset_color}%}'
+            else
+                # Show at most 1 leading and 2 trailing path components.
+                RPS1+=' %{${DEXTER_WORKDIR_COLOR}%}%(4~|%-1~/…/%2~|%3~)%{${reset_color}%}'
+            fi
+            ;;
+        host)
+            # Hostname.  Use the first component (upto the first period),
+            # truncated to 6 characters.
+            RPS1+=' %{${DEXTER_HOST_COLOR}%}%7>…>%m%>>%{${reset_color}%}'
+            ;;
+        time)
+            # Time in HH:MM 24-hour format.
+            RPS1+=' %{${DEXTER_TIME_COLOR}%}$(date +%H:%M)%{${reset_color}%}'
+            ;;
+        *)
+            echo "Invalid specifier '$elem' in $DEXTER_RIGHT_PROMPT_ELEMS." 1>&2
+            ;;
+    esac
+done
+# Remove leading space character.
+if [[ "$RPS1" =~ "^ " ]]; then
+    RPS1="${RPS1:1}"
 fi
 
-# Git status
-if type __git_ps1 >/dev/null 2>&1; then
-    GIT_PS1_SHOWDIRTYSTATE=yes
-    GIT_PS1_SHOWSTASHSTATE=yes
-    GIT_PS1_SHOWUNTRACKEDFILES=yes
-    GIT_PS1_SHOWUPSTREAM="verbose"
-    GIT_PS1_DESCRIBE_STYLE="branch"
-    RPS1+='%{${DEXTER_GIT_COLOR}%}$(__git_ps1 "(%s)")%{$reset_color%} '
-fi
 
-# Working directory.
-if type shrink_path >/dev/null 2>&1; then
-    # Use a fish-style abbreviation.
-    RPS1+='%{${DEXTER_WORKDIR_COLOR}%}$(shrink_path -l -t -T)%{$reset_color%} '
-else
-    # Show at most 1 leading and 2 trailing path components.
-    RPS1+='%{${DEXTER_WORKDIR_COLOR}%}%(4~|%-1~/…/%2~|%3~)%{$reset_color%} '
-fi
+# ----------  PREVIOUS COMMAND SUMMARY AND BLANK LINE  ----------
 
-# Hostname.
-# Use the first component (upto the first period), truncated to 6 characters.
-RPS1+='%{${DEXTER_HOSTNAME_COLOR}%}%7>…>%m%>>%{$reset_color%}'
+# Returns current time in seconds since Unix epoch.
+_get_timestamp() {
+    echo "$(date '+%s')"
+}
+
+# Initialize state variables, since `precmd` runs before `preexec` when the
+# shell starts.
+_start_seconds="$(_get_timestamp)"
+_is_timed_cmd="true"
+
+# Returns time formatted with suitable units.
+# $1: Time in seconds (integer).
+_format_time() {
+    local seconds="$1"
+    local time_msg
+    if [[ $seconds -lt 60 ]]; then
+        time_msg="${seconds}s"
+    elif [[ $seconds -lt 3600 ]]; then
+        time_msg="$((seconds / 60))m $((seconds % 60))s"
+    elif [[ $seconds -lt 86400 ]]; then
+        time_msg="$((seconds / 3600))h $((seconds % 3600 / 60))m"
+    else
+        time_msg="$((seconds / 86400))d $((seconds % 86400 / 3600))h"
+    fi
+    echo -n "$time_msg"
+}
+
+# Returns wall clock time elapsed since last call to `preexec` in suitable units,
+# formatted. The string is empty if less than 5 seconds have elapsed.
+# Args: None.
+_get_time_message() {
+    local stop_seconds="$(_get_timestamp)"
+    local seconds=$(( $stop_seconds - $_start_seconds ))
+    if [[ $seconds -ge "$DEXTER_TIME_LIMIT_SECS" ]] \
+           && [[ "${_is_timed_cmd}" == "true" ]]; then
+        echo -n "$(_format_time $seconds) elapsed"
+    fi
+}
+
+# Returns a formatted string showing the exit status of the last command.
+# $1: Numeric exit status of last command.
+_get_exit_message() {
+    local exit_message
+    if [[ $1 -eq 0 ]]; then
+        exit_message=""
+    elif [[ $1 -gt 128 ]] && [[ $1 -le 159 ]]; then
+        signal=$(($1 - 128))
+        case $signal in
+            2) exit_message="INTerrupted" ;;
+            9) exit_message="KILLed" ;;
+            15) exit_message="TERMinated" ;;
+            17) exit_message="" ;;
+            18) exit_message="" ;;
+            *) exit_message="SIGNAL $signal" ;;
+        esac
+    else
+        exit_message="exit $?"
+    fi
+    if [[ -n "$exit_message" ]]; then
+        exit_message="${DEXTER_EXIT_STATUS_COLOR}${exit_message}${reset_color}"
+    fi
+    echo -n "$exit_message"
+}
+
+# Prints a formatted string showing the exit status and elapsed wall-clock time
+# of the last command.
+# $1: Numeric exit status of last command.
+_print_postexec_message() {
+    local message=""
+    if [[ "$DEXTER_SHOW_EXIT_MESSAGE" == 1 ]]; then
+        message="${message}$(_get_exit_message "$1")"
+    fi
+    if [[ "$DEXTER_SHOW_ELAPSED_TIME" == 1 ]]; then
+        local time_message="$(_get_time_message)"
+        if [[ -n "$time_message" ]]; then
+            message="${message:+${message}; }${time_message}"
+        fi
+    fi
+    if [[ -n "$message" ]]; then
+        echo -e "(${message})"
+    fi
+}
+
+# This hook is called before the next prompt is displayed. It shows the exit
+# status of the last command and/or the wall-clock time it consumed, if
+# needed. Prints a blank line before the next prompt.
+# Args: None.
+precmd() {
+    _print_postexec_message $?
+    echo
+}
+
+# This hook is called after a command is entered and before it is executed. It
+# records the start time of the command to be executed.
+# $1: Command string as typed by the user.
+# $2: First line of the command, after alias expansion.
+# $3: Full command, after alias expansion.
+preexec() {
+    if [[ "$DEXTER_SHOW_ELAPSED_TIME" == 1 ]]; then
+        for cmd in "$DEXTER_UNTIMED_COMMANDS[@]"; do
+            if [[ $3 =~ "^${cmd}" ]]; then
+                _is_timed_cmd="false"
+                return
+            fi
+        done
+        _is_timed_cmd="true"
+        _start_seconds="$(_get_timestamp)"
+    fi
+}
